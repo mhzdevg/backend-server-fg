d9e3a06108c28d247f1600a4a7620cf1b5a9166c
mhzdevg <152190573+mhzdevg@users.noreply.github.com>
2023-11-30 01:26:44 +0500

    Message Updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB Connected, Your Flame Grill API is now listening`);
+    console.log(`MongoDB Connected, Flame Grill API is now listening`);
   } catch (error) {
     console.log(error);
     process.exit(1);
